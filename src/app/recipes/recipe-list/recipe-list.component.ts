56a725548a49830fac9c37e37548a8129686be14
DamTuanPhat <damtuanphat@gmail.com>
2022-07-25 13:15:20 +0700

    Add ingredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a simply test', 'https://get.pxhere.com/photo/omelet-dish-food-cuisine-ingredient-produce-meal-vegetarian-food-frittata-comfort-food-recipe-quiche-vegetable-vegan-nutrition-cookware-and-bakeware-brunch-garnish-curry-Bobotie-1620847.jpg'),
+    new Recipe('A test recipe', 'This is a simply test', 'https://get.pxhere.com/photo/omelet-dish-food-cuisine-ingredient-produce-meal-vegetarian-food-frittata-comfort-food-recipe-quiche-vegetable-vegan-nutrition-cookware-and-bakeware-brunch-garnish-curry-Bobotie-1620847.jpg'),
+  ];
 
   constructor() { }
 

20d6e16ffba48fe48908378fd544e111f05638fa
DamTuanPhat <damtuanphat@gmail.com>
2022-07-26 16:36:46 +0700

    Add shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.module';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.module';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is a simply test', 'https://get.pxhere.com/photo/omelet-dish-food-cuisine-ingredient-produce-meal-vegetarian-food-frittata-comfort-food-recipe-quiche-vegetable-vegan-nutrition-cookware-and-bakeware-brunch-garnish-curry-Bobotie-1620847.jpg'),
     new Recipe('A test recipe', 'This is a simply test', 'https://get.pxhere.com/photo/omelet-dish-food-cuisine-ingredient-produce-meal-vegetarian-food-frittata-comfort-food-recipe-quiche-vegetable-vegan-nutrition-cookware-and-bakeware-brunch-garnish-curry-Bobotie-1620847.jpg'),
@@ -15,6 +17,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }

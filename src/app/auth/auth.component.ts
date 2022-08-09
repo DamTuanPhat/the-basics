5d102ad310cd84ecc63ff9b067b39a4f4b5a031c
DamTuanPhat <damtuanphat@gmail.com>
2022-08-09 10:59:13 +0700

    Section 20 - Authentication

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +11,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router,
+              private componentFactoryResolver: ComponentFactoryResolver
+              ) { }
 
   ngOnInit(): void {
   }
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(authForm: NgForm) {
+    if(!authForm.valid) {
+      return;
+    }
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+    if(this.isLoginMode) {
+      authObs = this.authService.login(email, password)
+    }
+    else{
+      authObs = this.authService.signup(email, password)
+    }
+
+    authObs.subscribe(response => {
+      console.log(response);
+      this.isLoading = false;
+      this.router.navigate(['/recipes']);
+    }, errorMessage => {
+      this.error = errorMessage;
+      this.showErrorAlert(errorMessage);
+      this.isLoading = false;
+    });
+
+    authForm.reset();
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  private showErrorAlert(message: string) {
+    // const alertCmp = new AlertComponent();
+    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+  }
 }
